7915286821a32a4e13f0fbe9ed0a1d887882a27c
tlggnd <tolga.gundogdu91@gmail.com>
2024-12-05 14:40:35 +0300

    asdasd

--- yonetim/template/navbar.php
+++ yonetim/template/navbar.php
@@ -61,8 +61,8 @@
 							</div>
 						</a>
 						<ul class="dropdown-menu dropdown-menu-end">
-							<!-- <li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-user fs-5"></i><span>Profile</span></a>
-							</li> -->
+							<li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-user fs-5"></i><span>Profile</span></a>
+							</li>
 							<li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-cog fs-5"></i><span>Şifre Ayarları</span></a>
 							</li>
 
